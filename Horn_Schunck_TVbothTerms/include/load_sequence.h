86ec9a7ceeede2e2aa4fd9ce20cb87c096b5d087
Denis Tananaev <d.d.tananaev@gmail.com>
2016-09-16 13:13:16 +0100

    added TVbothTerms

---
+++ Horn_Schunck_TVbothTerms/include/load_sequence.h
@@ -0,0 +1,9 @@
+#ifndef LOAD_SEQUENCE_H_
+#define LOAD_SEQUENCE_H_
+#include <CVector.h>
+#include <CMatrix.h>
+#include <string>
+
+CVector< CMatrix<float> > loadSequence(std::string Filename);
+
+#endif
